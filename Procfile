e60aed459765b1ef1ccc454d789b93dac5205e37
Kriti Sharma <Kriti@Kritis-MacBook-Pro.local>
2017-09-28 23:02:43 +0530

    env port

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node index.js
+web: PORT=9999 node index.js 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mysql = require('mysql')
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000;
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -28,14 +28,14 @@ var con = mysql.createConnection({
 // 	  response.send('Ok')
 // })
 
-app.get('/summer', (request, response) => {
+app.get('/', (request, response) => {
 	  response.send('Ok')
 })
 
-app.listen(port, (err) => {
+app.listen(PORT , (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
 
-  console.log(`server is listening on ${port}`)
+  console.log(`Server is listening on port ${PORT}`);
 })
